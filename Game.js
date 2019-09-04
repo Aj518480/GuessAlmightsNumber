d5fcbd3b479f4f39b0d70ea192790a1282137eca
aj518480 <allisoncordovez@gmail.com>
2019-09-04 18:16:38 -0400

    added jquery to link to html pg, created a start game function to put up numbers when started. Also created .on click functions for imgs to generate random numbers

--- Game.js
+++ Game.js
@@ -1,113 +1,77 @@
 
+$(document).ready(function() {
 
-
-    
-    // Keep current score board
+   // Keep current score board
     //score will go up in a win when almighNumber === studentValue
     var wins = 0;
-
     // score will go up in losses when studentValue > almightNumber
     //reset displays once lose happens
     var losses = 0;
-
     //student value will change everytime the game starts
-    //each student will have a different value from 1-9
+    //each student will have a different value from 1-12
     var studentOne;
     var studentTwo;
     var studentThree;
     var studentFour;
-
     //randomnumber will go into here
-
     var almightNumber;
-
     //Gamers added total
     var gamerScore;
+    
 
 
     //create a function to display the results to the pg//
     function updateDisplay() {
        
-        document.querySelector("#almightNumber").innerHTML = almightNumber;
-        document.querySelector("#wins").innerHTML = wins;
-        document.querySelector("#losses").innerHTML = losses;
-        document.querySelector("#gamerScore").innerHTML = gamerScore;
+        $("#almightNumber").text(almightNumber);
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        $("#gamerScore").text(gamerScore);
     };
 
     updateDisplay();
-
-   
     
     //create a function to get a random number for almight
-    //Might have to fix so it randomize a number
-    function randomNumber() {
-        almightNumber = randomlySelectedNumber [Math.floor(Math.random() * randomlySelectedNumber.length)];
-    };
-    $( "friendOne" ).click(function() {
-        randomNumber();
-        
+    
+   function startGame(){
+       gamerScore=0;
 
-   
-    //function to reset my gamer score
-    //happens after gamerscore > almight
-    function resetScores() {
-        gamerScore;
-    }
+       almightNumber = Math.floor(Math.random() *(120-19) + 19);
+       studentOne = Math.floor(Math.random() * 12) + 1;
+       studentTwo = Math.floor(Math.random() * 12) + 1;
+       studentThree = Math.floor(Math.random() * 12) + 1;
+       studentFour= Math.floor(Math.random() * 12) + 1;
 
-//learn to connect JS to Index.html
-//Cleaned up code until here will continue back HERE!!!!
+       $("#almightNumber").text(almightNumber);
+       $("#gamerScore").text(gamerScore);
 
+   } 
+  startGame();
 
+  $("#friendOne").on("click",function(){
+    gamerScore += studentOne;
+    $("#friendOne").text(gamerScore);
+    updateDisplay();
+  });
 
+  $("#friendTwo").on("click",function(){
+    gamerScore += studentTwo;
+    $("#friendTwo").text(gamerScore);
+    updateDisplay();
+  });
 
-randomNumber()
+  $("#friendThree").on("click",function(){
+    gamerScore += studentThree;
+    $("#friendThree").text(gamerScore);
+    updateDisplay();
+  });
 
-    console.log(almightNumber)
-
-    //create a onkey up event function to grab UserGuess
-    document.onkeyup = function (event) {
-
-        gamerScore = event.key;
-
-        console.log(userGuess);
-
-
-        guessSoFar.push(userGuess);
-        //userguesses are now being pushed into guessSofar
-
-
-        if (userGuess === computerGuess) {
-            wins++;
-            resetScores();
-            computerRandomLetter();
-            document.querySelector("#wins").innerHTML = wins;
-            displayText = "hey you won!";
-            updateDisplay();
-            //win will go up 1//
-            //guesses go back to 9 to restart
-            //computer will generate another guess
-            //display should change to "you won!"
-        }
-      if (userGuess !== computerGuess) {
-            guessLeft--;
-            guessSoFar.push(userGuess);
-            updateDisplay();
-            //guesses should be 1 less than it was
-            //user guess should show in guesses so far span
-
-        }
-
-        if (guessLeft === 0) {
-            resetScores()
-            losses++;
-            displayText = "you lost!";
-            computerRandomLetter();
-            updateDisplay();
-        }
-        //once it gets down to 0 reset
-        //lose will go up 1//
-
-        updateDisplay();
+  $("#friendFour").on("click",function(){
+    gamerScore += studentFour;
+    $("#friendFour").text(gamerScore);
+    updateDisplay();
+  });
 
 
-    };
+
+});
